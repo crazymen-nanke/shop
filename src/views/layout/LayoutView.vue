bdaf16d8cff09feb6ad987fd41fc32d2622a0b2a
Simon <nk@crazymen.cn>
2023-01-16 00:43:48 +0800

    使用使用Element Plus页面布局

--- src/views/layout/LayoutView.vue
+++ src/views/layout/LayoutView.vue
@@ -7,7 +7,9 @@
                     <router-link to="/index">角色列表</router-link>
                     <router-link to="/users">用户列表</router-link>
                 </el-aside>
-                <el-main><router-view /></el-main>
+                <el-main>
+                    <router-view></router-view>
+                </el-main>
             </el-container>
         </el-container>
     </div>
