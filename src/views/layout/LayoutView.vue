d0ddd380ae56ddf6129cfbdbaf01f5ab56658d99
Simon <nk@crazymen.cn>
2023-01-15 22:19:30 +0800

    安装Element Plus

--- src/views/layout/LayoutView.vue
+++ src/views/layout/LayoutView.vue
@@ -1,13 +1,18 @@
 <template>
-    <div>
-        <h1>我是导航
-            <router-link to="/index">角色列表</router-link>
-            <router-link to="/users">用户列表</router-link>
-        </h1>
-        <h1>我是侧边栏</h1>
-        <router-view />
+    <div class="common-layout">
+        <el-container>
+            <el-header>Header</el-header>
+            <el-container>
+                <el-aside width="200px">
+                    <router-link to="/index">角色列表</router-link>
+                    <router-link to="/users">用户列表</router-link>
+                </el-aside>
+                <el-main><router-view /></el-main>
+            </el-container>
+        </el-container>
     </div>
 </template>
+
 
 <script>
 
